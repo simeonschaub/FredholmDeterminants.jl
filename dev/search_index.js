291dfdb985e90a220b3ea40a1bb2a84567cfba9d
Documenter.jl <documenter@juliadocs.github.io>
2025-02-25 21:14:05 +0000

    build based on 0bf4194

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FredholmDeterminants","category":"page"},{"location":"#FredholmDeterminants","page":"Home","title":"FredholmDeterminants","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FredholmDeterminants.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FredholmDeterminants]","category":"page"}]
+}
